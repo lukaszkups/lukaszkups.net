65d672a320d5b0f514e3d4729c736a94ca546a73
mrmnmly <mrmnmly@gmail.com>
2018-12-30 13:55:12 +0100

    New init function.

--- lukaszkups.net/index.js
+++ lukaszkups.net/index.js
@@ -1,13 +1,4 @@
 const content = require('./cms/content')
 const _store = require('./cms/store')
 
-content.getAllContent(_store).then(store => {
-  // console.log(store)
-  // console.log('-------')
-  // console.log(store.lists[0])
-  content.createOutputFolders(_store).then(() => {
-    content.createOutputFiles(_store).then(() => {
-      console.log('done.')
-    })
-  })
-})
+content.init(_store)
