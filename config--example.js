f9eae217a715e81af45bd738f841b52d2f0c5ed6
mrmnmly <mrmnmly@gmail.com>
2019-02-09 16:04:42 +0100

    Added example ftp deploy config.

--- config--example.js
+++ config--example.js
@@ -11,5 +11,15 @@ module.exports = {
   paginationSlug: 'page/',
   pagination: {
     notes: 20
+  },
+  deployViaFtp: true, // if you want to deploy your files via writte.li itself via deploy command
+  ftpConfig: {
+    user: '<your ftp user name>',
+    password: '<your ftp password>',
+    host: '<your ftp host e.g. ftp.goooogle.com>',
+    localRoot: './output', // don't change it, it points directly to the source folder of generated website
+    remoteRoot: '/<path to the folder on your ftp server>/',
+    deleteRemote: false, // if you want always to remove old contents in ftp, change it to true
+    forcePasv: true // ftp will force to run in passive mode (EPSV command is not sent)
   }
 }
