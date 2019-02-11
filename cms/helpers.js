77a5d7b509c89967e440cae0356e669810dada2e
mrmnmly <mrmnmly@gmail.com>
2019-02-11 23:29:03 +0100

    Added simplebar.

--- cms/helpers.js
+++ cms/helpers.js
@@ -143,7 +143,7 @@ module.exports = {
   },
   startServer: () => {
     const server = http.createServer({root: './output/'})
-    server.listen(3000)
+    server.listen(3001)
     console.log('Output folder is now served under http://localhost:3000')
   },
   deployViaFtp: () => {
