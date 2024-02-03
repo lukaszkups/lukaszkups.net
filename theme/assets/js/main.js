6cef162fbefc201e0352dc555876df819990402f
lukaszkups <lukaszkups@gmail.com>
2024-02-03 20:58:41 +0100

    Started working on website layout.

--- theme/assets/js/main.js
+++ theme/assets/js/main.js
@@ -1,1 +1,21 @@
 console.info('It works!');
+
+// Update current year dates automatically 😎
+const updateYears = () => {
+  const nodes = document.querySelectorAll('.lk-update-year');
+  const year = new Date().getFullYear();
+  nodes.forEach((item) => {
+    item.innerHTML = year;
+  });
+}
+updateYears();
+
+// Calculate how much time has passed since [data-since-year] value 👨‍💻
+const calculateTimespan = () => {
+  const nodes = document.querySelectorAll('.lk-calculate-timespan')
+  const year = new Date().getFullYear();
+  nodes.forEach((item) => {
+    item.innerText = year - parseInt(year.getAttribute("data-since-year"));
+  });
+}
+calculateTimespan();
--- theme/templates/home.js
+++ theme/templates/home.js
@@ -1,7 +1,12 @@
 import renderLayout from "./layout.js";
 
 const renderHome = (contentData) => {
-  return `<div>${JSON.stringify(contentData)}</div>`;
+  return `
+    <div>${JSON.stringify(contentData)}</div>
+    <div class="hero-wrapper">
+      <div class="hero-avatar-image"></div>
+    </div>
+  `;
 }
 
 const render = (contentData) => {
