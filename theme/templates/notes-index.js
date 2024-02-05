d556a984fb24ea334ae6246e41f9b51537ac5295
lukaszkups <lukaszkups@gmail.com>
2024-02-05 19:28:13 +0100

    Updated root notes layout.

--- theme/templates/notes-index.js
+++ theme/templates/notes-index.js
@@ -1,7 +1,23 @@
 import renderLayout from "./layout.js";
 
 const renderNotesIndex = (contentData) => {
-  return `<div>${JSON.stringify(contentData)}</div>`;
+  return `
+    <div class="notes-index-wrapper">
+      <h2 class="bebas">notes<span>_</span></h2>
+      <div id="particles-js--gold"></div>
+    </div>
+    <ul class="notes-years-wrapper bebas">
+      <li><a href="/notes/2024/">2024</a><span></span></li>
+      <li><a href="/notes/2023/">2023</a><span></span></li>
+      <li><a href="/notes/2022/">2022</a><span></span></li>
+      <li><a href="/notes/2021/">2021</a><span></span></li>
+      <li><a href="/notes/2020/">2020</a><span></span></li>
+      <li><a href="/notes/2019/">2019</a><span></span></li>
+      <li><a href="/notes/2018/">2018</a><span></span></li>
+      <li><a href="/notes/2017/">2017</a><span></span></li>
+      <li><a href="/notes/2015/">2015</a><span></span></li>
+    </ul>
+  `;
 }
 
 const render = (contentData) => {
